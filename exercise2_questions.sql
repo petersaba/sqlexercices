b08de663da1ebed66fd1865ffe782840b519e441
Peter Saba <petersaba001@gmail.com>
2022-09-11 20:54:14 +0300

    finished 4th query

--- exercise2_questions.sql
+++ exercise2_questions.sql
@@ -9,3 +9,7 @@ WHERE start_time = (SELECT MIN(start_time) FROM courses);
 SELECT c.crn, c.name
 FROM courses AS c, enrolls AS e, majors AS m, departments AS d
 WHERE d.name = "BIF" and d.id = m.dept_id and m.student_id = e.student_id and e.course_crn and e.course_crn = c.crn;
+
+SELECT *
+FROM students
+WHERE id NOT IN (SELECT DISTINCT student_id FROM enrolls);
