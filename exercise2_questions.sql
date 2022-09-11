b71f7ab04509d1e57705074caeba6712971642af
Peter Saba <petersaba001@gmail.com>
2022-09-11 21:17:25 +0300

    finished 6th query

--- exercise2_questions.sql
+++ exercise2_questions.sql
@@ -17,3 +17,7 @@ WHERE id NOT IN (SELECT DISTINCT student_id FROM enrolls);
 SELECT COUNT(e.student_id)
 FROM students AS s, enrolls AS e, courses AS c, majors AS m, departments AS d
 WHERE c.name = 'CSC275' and c.crn = e.course_crn and e.student_id = s.id and s.id = m.student_id and m.dept_id = d.id and d.name = 'CS';
+
+SELECT COUNT(DISTINCT e.student_id)
+FROM students AS s, enrolls AS e, courses AS c, majors AS m, departments AS d
+WHERE d.name = 'CS' and d.id = m.dept_id and m.student_id = s.id and s.id = e.student_id;
