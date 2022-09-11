ab6c36f031f45d0173e39b0af108e1a747adff7d
Peter Saba <petersaba001@gmail.com>
2022-09-11 20:32:23 +0300

    finished 3rd query

--- exercise2_questions.sql
+++ exercise2_questions.sql
@@ -5,3 +5,7 @@ WHERE capacity > 100;
 SELECT *
 FROM courses
 WHERE start_time = (SELECT MIN(start_time) FROM courses);
+
+SELECT c.crn, c.name
+FROM courses AS c, enrolls AS e, majors AS m, departments AS d
+WHERE d.name = "BIF" and d.id = m.dept_id and m.student_id = e.student_id and e.course_crn and e.course_crn = c.crn;
